1e44debf8dafc875a15062c50f02a087e3413750
Mostakimul Karim <mk.mohit7574@gmail.com>
2022-03-14 23:32:56 +0100

    [add]: product details service added

--- src/services/Product.service.ts
+++ src/services/Product.service.ts
@@ -5,6 +5,11 @@ class ProductService {
     const { data } = await httpReq.get("/product");
     return data;
   }
+
+  async getProductDetailsById(id: string): Promise<IProduct> {
+    const { data } = await httpReq.get(`/product/${id}`);
+    return data;
+  }
 }
 
 export default new ProductService();
